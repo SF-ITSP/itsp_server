ca8613470792ed6dcdc8dcd85f5dc7d5d752b722
zhengpingling <1037956211@qq.com>
2016-05-30 17:17:30 +0800

    [zpl & hjy] finished vehicle and task unit test

--- src/test/java/com/itsp/supplier/controller/TaskControllerTest.java
+++ src/test/java/com/itsp/supplier/controller/TaskControllerTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.itsp.common.ItspJUnit4ClassRunner;
 import com.itsp.common.dao.JdbcDao;
@@ -17,6 +19,8 @@ public class TaskControllerTest extends ItspJUnit4ClassRunner {
 	private JdbcDao jdbcDao;
 
 	@Test
+	@Rollback
+	@Transactional
 	public void should_find_all() {
 
 		// give
@@ -24,12 +28,11 @@ public class TaskControllerTest extends ItspJUnit4ClassRunner {
 		jdbcDao.getJdbcTemplate()
 				.execute("insert into TT_ITSP_TASK(id,address,operation,arrive_time,latest_departure_time,waiting_time)"
 						+ "values (-1,'广州中转场','1',timestamp'2016-01-01 12:13',timestamp'2016-01-01 09:56',20)");
-
+		//when
 		List<Task> tasks = taskController.getTasks();
-
 		Task task = tasks.get(0);
+		//then
 		Assert.assertEquals(1, tasks.size());
-
 		Assert.assertEquals("广州中转场", task.getAddress());
 		Assert.assertEquals("卸车", task.getOperation());
 		Assert.assertEquals("12:13", task.getArriveTime());
---
+++ src/test/java/com/itsp/supplier/controller/VehicleControllerTest.java
@@ -0,0 +1,45 @@
+package com.itsp.supplier.controller;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.itsp.common.ItspJUnit4ClassRunner;
+import com.itsp.common.dao.JdbcDao;
+import com.itsp.supplier.entity.Vehicle;
+
+public class VehicleControllerTest extends ItspJUnit4ClassRunner {
+	
+	@Autowired
+	private VehicleController vehicleController;
+	
+	@Autowired
+	private JdbcDao jdbcDao;
+
+	@Test
+	@Rollback
+	@Transactional
+	public void should_find_by_carrier_id() {
+
+		// give
+		jdbcDao.getJdbcTemplate().execute("delete from TT_ITSP_VEHICLE");
+		jdbcDao.getJdbcTemplate()
+				.execute("INSERT INTO TT_ITSP_VEHICLE(id,carrier_id,vehicle_number,type,weight)"
+						+ " values(-2,888,'粤B123456',1,14)");
+		//when
+		List<Vehicle> vehicles = vehicleController.getVehicle(888);
+		Vehicle vehicle = vehicles.get(0);
+		//then
+		Assert.assertEquals(1, vehicles.size());
+		Assert.assertEquals(888, vehicle.getCarrierId());
+		Assert.assertEquals("粤B123456", vehicle.getVehicleNumber());
+		Assert.assertEquals("其他", vehicle.getType());
+		Assert.assertEquals(14, vehicle.getWeight());
+
+	}
+
+}
